aa12926f0b71e8e4215364b9dd09d9ef8e95fbf9
Student20022 <131999657+Student20022@users.noreply.github.com>
2024-04-22 23:36:32 +0300

    Add files via upload

---
+++ лаб_3/лаб_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace BuildingSiteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "server=localhost;database=BuildingSiteDB;uid=root;password=mysqlshit";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connected to the database.");
+
+                    // Выполнение запроса к таблице Suppliers
+                    string query = "SELECT * FROM Suppliers";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("Supplier_id\tSupplier_name\tAddress\tPhone\tManager_lastname");
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader.GetInt32(0)}\t{reader.GetString(1)}\t{reader.GetString(2)}\t{reader.GetString(3)}\t{reader.GetString(4)}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
